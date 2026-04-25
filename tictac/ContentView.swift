b1586b330a27aa41d4a0e19221fbd85df7e20038
Dave Glover <dglover@microsoft.com>
2026-04-25 12:49:55 +1000

    Initial Commit

---
+++ tictac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tictac
+//
+//  Created by David Glover on 25/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tictac/tictacApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictacApp.swift
+//  tictac
+//
+//  Created by David Glover on 25/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct tictacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
